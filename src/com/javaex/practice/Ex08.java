93664259d9c3492ba36e90f8890051f18d58f6c0
가은 <rkdms8228@naver.com>
2022-04-20 18:16:43 +0900

    ex08

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		int[] lotto = new int[6];
+		
+		for(int i = 0; i<lotto.length; i++) {
+			
+			lotto[i] = (int)(Math.random()*45)+1;
+			System.out.print(lotto[i]+"\t");
+			
+		}
+		
+	}
+
+}
